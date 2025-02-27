fbb86e7d81752e1f8f52b70b5dde02383d7ab98d
Zeph LaBoden <zlabodn@gmail.com>
2025-02-27 16:03:11 +0000

    updating readme, adding config

--- ip_geo_proxy.php
+++ ip_geo_proxy.php
@@ -1,9 +1,41 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
-$API_KEY = "efaebf5eeae541019722aba565851296";
 
-function get_geolocation($apiKey, $ip, $lang = "en", $fields = "state_prov") {
-    $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip."&lang=".$lang."&fields=".$fields;
+$ERROR_INVALID_IP = '`ip_address` is invalid';
+$ERROR_MISSING_API_KEY = '`API_KEY` is required';
+$ERROR_MISSING_IP = '`ip_address` is required';
+$API_KEY_NAME = "IP_GEO_KEY";
+
+function get_api_key() {
+    global $API_KEY_NAME;
+
+    $lines = file(__DIR__."/.env", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+
+        if ($name === $API_KEY_NAME) {
+            return $value;
+        }
+    }
+}
+
+function get_geolocation($api_key, $ip, $lang = "en", $fields = "state_prov") {
+    global $ERROR_MISSING_API_KEY;
+
+    if (!$api_key) {
+        http_response_code(500);
+        return json_encode([
+            'error_code' => 5002,
+            'error_message' => $ERROR_MISSING_API_KEY,
+        ]);
+    }
+    $url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$api_key."&ip=".$ip."&lang=".$lang."&fields=".$fields;
     $cURL = curl_init();
     curl_setopt($cURL, CURLOPT_URL, $url);
     curl_setopt($cURL, CURLOPT_HTTPGET, true);
@@ -13,23 +45,26 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "state_prov") {
         'Accept: application/json',
         'User-Agent: '.$_SERVER['HTTP_USER_AGENT']
     ));
-    $response = [];
+
     try {
         return curl_exec($cURL);
     } catch (Exception $e) {
-        var_dump($e);
-        $response = $e;
+        http_response_code(500);
+        return json_encode([
+            'error_code' => 5001,
+            'error_message' => $e->getMessage(),
+        ]);
     }
-    return json_decode($response);
 }
 
+$API_KEY = get_api_key();
 $remote_ip = $_GET['ip_address'] ?? false;
 
 if (!$remote_ip) {
     http_response_code(400);
     echo json_encode([
         'error_code' => 4001,
-        'error' => '`ip_address` is required'
+        'error_message' => $ERROR_MISSING_IP
     ]);
     return;
 }
@@ -38,7 +73,7 @@ if (filter_var($remote_ip, FILTER_VALIDATE_IP) == false) {
     http_response_code(400);
     echo json_encode([
         'error_code' => 4002,
-        'error' => '`ip_address` is invalid'
+        'error_message' => $ERROR_INVALID_IP
     ]);
     return;
 }
