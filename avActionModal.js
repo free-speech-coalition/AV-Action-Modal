f53c2bee0e4ad6b8792948b7782b1dc972bc28c8
J Tro <it@tim-global.com>
2025-05-08 18:39:38 -0700

    Adjust modal JS for cookie access within iframes. Add multiple security measures for CORS, etc. Remove as many global variables as possible in favor of defined constants.

--- avActionModal.js
+++ avActionModal.js
@@ -30,7 +30,8 @@ document.addEventListener("DOMContentLoaded", async function () {
     function setAVCookie(name, value, days) {
         const expires = new Date();
         expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-        document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; SameSite=Lax`;
+        // "Lax" prevents embedded content (iframes) from setting the cookie.
+        document.cookie = `${name}=${value}; expires=${expires.toUTCString()}; path=/; Secure; SameSite=None`;
     }
 
     // Function to fetch user's location
